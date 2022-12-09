416a43358b28b005790cc83d77e6e1134f06e6c8
saif ali <saif.ali40@gmail.com>
2022-12-09 20:19:36 +0530

    patch for the last updated time

--- domains/Reports.go
+++ domains/Reports.go
@@ -1,17 +1,20 @@
 package domain
 
+import "time"
+
 type Reports struct {
-	Id           string          `firestore:"id" json:"id"`
-	Contact      *Contact        `firestore:"contact" json:"contact"`
-	Award        []*Awards       `firestore:"award" json:"award"`
-	Certificates []*Certificates `firestore:"certificates" json:"certificates"`
-	Educations   []*Educations   `firestore:"educations" json:"educations"`
-	Interests    []*Interests    `firestore:"interests" json:"interests"`
-	Languages    []*Languages    `firestore:"languages" json:"languages"`
-	Projects     []*Projects     `firestore:"projects" json:"projects"`
-	Publications []*Publications `firestore:"publications" json:"publications"`
-	References   []*References   `firestore:"references" json:"references"`
-	Skills       []*Skills       `firestore:"skills" json:"skills"`
-	Volunteers   []*Volunteers   `firestore:"award" json:"volunteers"`
-	Works        []*Works        `firestore:"timelines" json:"works"`
+	Id               string          `firestore:"id" json:"id"`
+	Contact          *Contact        `firestore:"contact" json:"basics"`
+	Award            []*Awards       `firestore:"award" json:"award"`
+	Certificates     []*Certificates `firestore:"certificates" json:"certificates"`
+	Educations       []*Educations   `firestore:"educations" json:"educations"`
+	Interests        []*Interests    `firestore:"interests" json:"interests"`
+	Languages        []*Languages    `firestore:"languages" json:"languages"`
+	Projects         []*Projects     `firestore:"projects" json:"projects"`
+	Publications     []*Publications `firestore:"publications" json:"publications"`
+	References       []*References   `firestore:"references" json:"references"`
+	Skills           []*Skills       `firestore:"skills" json:"skills"`
+	Volunteers       []*Volunteers   `firestore:"volunteers" json:"volunteers"`
+	Works            []*Works        `firestore:"works" json:"works"`
+	LastModifiedDate time.Time       `firestore:"lastModifiedDate" json:"lastModifiedDate" example:"1658340160"`
 }
