722ae3ef5e5c5cdb849511dc4c8348d9ecfc4763
saif ali <saif.ali40@gmail.com>
2022-12-09 20:57:23 +0530

    updated the preferences

--- domains/preference.go
+++ domains/preference.go
@@ -9,15 +9,15 @@ type Protfolio struct {
 }
 
 type Specific struct {
+	DreamJob   DreamJob `firestore:"dreamJob" json:"dreamJob"`
+	CurrentJob *Works   `firestore:"currentJob" json:"currentJob"`
+	IsPrivate  bool     `firestore:"isPrivate" json:"isPrivate"`
+}
+
+type DreamJob struct {
 	Locations       []Location `firestore:"locations" json:"locations"`
 	RemoteFrequency []string   `firestore:"remoteFrequency" json:"remoteFrequency"`
 	Type            []string   `firestore:"jobType" json:"jobType"`
 	Company         []string   `firestore:"company" json:"company"`
 	ExpirienceLevel []string   `firestore:"experience" json:"experience"`
-	CurrentJob      *Works     `firestore:"currentJob" json:"currentJob"`
-}
-
-type DreamJob struct {
-	Locations       []Location `firestore:"locations" json:"locations"`
-	RemoteFrequency string     `firestore:"remoteFrequency" json:"remoteFrequency"`
 }
