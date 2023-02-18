2cf84087b0fb7e894d6f493fe647c2601660baec
saif ali <saifali@FULL852-PRO.local>
2023-02-18 17:22:23 +0530

    updated the entity for location operations

--- domains/profile.go
+++ domains/profile.go
@@ -7,9 +7,11 @@ type Profiles struct {
 }
 
 type Location struct {
+	Id              string `json:"id" example:"asdasdsa1"`
 	Address         string `json:"address" example:"Acme corp"`
 	PostalCode      string `json:"postalCode" example:"123456"`
 	CountryCode     string `json:"countryCode" example:"LD"`
 	Region          string `json:"region" example:"LD"`
+	City            string `json:"city" example:"palakkad"`
 	VisaSponsorship bool   `json:"visaSponsorship"`
 }
